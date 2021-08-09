ca9a0279019b0d908ecd9b40e342ea8ac63e25d2
Agustin625 <marcoagusfernandez@gmail.com>
2021-08-09 19:59:39 -0300

    clase error

---
+++ src/main/java/ProyectoEgg/argus/Entidades/usario.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProyectoEgg.argus.Entidades;
+
+/**
+ *
+ * @author Agustin
+ */
+public class usario {
+    
+}
