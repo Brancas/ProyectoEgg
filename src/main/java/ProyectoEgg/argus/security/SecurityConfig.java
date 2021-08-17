3e6ddbcf235c935e853aba27ea9955a7236f1bf0
Agustin625 <marcoagusfernandez@gmail.com>
2021-08-17 20:12:07 -0300

    resolver conflictos

--- src/main/java/ProyectoEgg/argus/security/SecurityConfig.java
+++ src/main/java/ProyectoEgg/argus/security/SecurityConfig.java
@@ -1,34 +1,9 @@
-<<<<<<< HEAD
 
-package ProyectoEgg.argus.security;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- *
- * @author Agustin
- */
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-    
-    @Override
-    protected  void configure (HttpSecurity http) throws Exception{
-        http.authorizeRequests()
-            .antMatchers("/css/*", "/js/*", "/img/*", "/**")
-                .permitAll()
-                .and()
-                .csrf()
-                .disable();
-   
-    }
     
     
     
-    
-}
-=======
 
 package ProyectoEgg.argus.security;
 
@@ -54,4 +29,3 @@ public class ConfiguracionesSeguridad extends WebSecurityConfigurerAdapter{
         }   }
 }
 
->>>>>>> Gustavo-Marianetti
